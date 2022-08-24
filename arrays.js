4bcfd6e049a483ec6a02c28cb2cab064cdc8a976
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-08-24 21:51:15 +0530

    array methods

---
+++ arrays.js
@@ -0,0 +1,23 @@
+let arr=[1,2,4,5,6,7,8,9,10];
+// console.log(arr.length);
+// console.log(arr[3]);
+// arr[3]=99; //edit array using indexx number
+// console.log(arr[3]);
+// arr.push("ppr");//pushes ele at end of array
+// console.log(arr);
+//console.log(arr.slice(1,8)); // to get sub array from particular index to particular index
+// arr.splice(2,4,"pra","pgr");//insert at index and remove ele from array
+// console.log(arr);
+// let numarr=[1,2,3,4,5]
+// console.log(arr.concat(numarr));//concatenation of arrays
+//console.log(arr + numarr); //this will treat every as a string
+console.log(arr);
+//console.log(arr.copyWithin(0,4,9));// (target_index,copyfrom_index, to_index );
+console.log(arr.includes(5,3));//(ele ,at_index)
+console.log(arr.indexOf(5));
+//console.log(arr.map());
+console.log(arr.reverse());
+console.log(arr.sort());
+console.log(arr.toString());
+let x="ppppppp";
+console.log(x.split(""));
---
+++ switch.js
@@ -0,0 +1,14 @@
+let user="prep";
+switch(user){
+    case "admin":
+        console.log("your are admin");
+        break;
+    case "student":
+        console.log("you are student");
+        
+    case "tester":
+        console.log("you are tester");    
+    default:
+        console.log("not a user");
+
+}
